914346724c1e3322f7236db46d566266c9a30639
Alishbah Farooq <alishbahfarooq@Alishbahs-MacBook-Air.local>
2026-06-28 23:38:27 -0600

    Loading unusual data

--- analyzing_data.py
+++ analyzing_data.py
@@ -1,1 +1,6 @@
-
+# Reading the unusual data into a list of lists
+with open('unusual_data.txt') as f:
+    unusual_data = [ ]
+    for line in f: # going report by report
+        if line.strip(): # skipping any blank reports in the data
+            unusual_data.append(list(map(int, line.split()))) # appending each report to ful list
