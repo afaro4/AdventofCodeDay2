ac81b6391a8ef12516befe534d9d7bb71d3a85ff
Alishbah Farooq <alishbahfarooq@Alishbahs-MacBook-Air.local>
2026-06-29 00:32:19 -0600

    Checking data for safe reports

--- analyzing_data.py
+++ analyzing_data.py
@@ -4,3 +4,38 @@ with open('unusual_data.txt') as f:
     for line in f: # going report by report
         if line.strip(): # skipping any blank reports in the data
             unusual_data.append(list(map(int, line.split()))) # appending each report to ful list
+
+# Checking each report safe or unsafe
+#sample_data = unusual_data[0:3]
+num_safe_reports = 0 # initiating counter for number of safe reports
+for report in unusual_data:
+    print(report)
+    for i in range(0, len(report)):
+        print("INDEX:"+ str(i))
+        if (i == len(report)-1): # if its at the last level then it is safe
+            print("SAFE")
+            num_safe_reports += 1
+        else: # if we are not at the last level of the report
+            difference = report[i] - report[i+1]
+            print(abs(difference))
+            # check if numbers are increasing or decreasing
+            if i == 0:
+                if difference < 0:
+                    direction = 1 # numbers increasing
+                else:
+                    direction = -1 # numbers decreasing
+            else: # move to next report if the proceeding numbers are not following the same pattern
+                if (direction == 1) & (difference > 0):
+                    break
+                if (direction == -1) & (difference < 0):
+                    break
+
+            # check if the differences are between 1 and 3
+            if 0 < abs(difference) < 4: 
+                continue
+            else:
+                print("UNSAFE")
+                break
+
+# Printing answer
+print("There are " + str(num_safe_reports) + " safe reports in the data.")
